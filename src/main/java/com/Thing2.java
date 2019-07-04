82ac07298e08e06fc4fdfba6eff27bce186fbf25
HarrisonVanNorth <hgvannort@gmail.com>
2019-07-03 18:39:38 -0700

    check point one

---
+++ src/main/java/com/Thing2.java
@@ -0,0 +1,7 @@
+package com;
+
+public class Thing2 {
+
+        public static void main(String[] args) {
+        }
+}
--- src/main/java/com/application.java
+++ src/main/java/com/application.java
@@ -2,6 +2,15 @@ package com;
 
 public class application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int total = 0;
+
+        for(int i = 0; i < args.length; i++) {
+            int x = Integer.parseInt(args[i]);
+            total += x;
+        }
+        System.out.println(total);
     }
+
+
 }
+
